aab4e0b8c38db17bcbcb70c56d72c4b51b07ed90
chenhong <924010725@qq.com>
2014-10-30 13:26:42 +0800

    上传

--- app/src/main/java/com/banner/main/Banner.java
+++ app/src/main/java/com/banner/main/Banner.java
@@ -18,7 +18,7 @@ public class Banner extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.banner, menu);
+        getMenuInflater().inflate(R.menu.menu_banner, menu);
         return true;
     }
 
